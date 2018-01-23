9536bb6f0623371116f72208122ad7ed4108820b
saneyee sarkar <saneyeesarkar@gmail.com>
2018-01-23 11:40:36 -0800

    remove env from git ignore

---
+++ Texter/Models/EnvironmentVariables.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace Texter.Models
+{
+    public static class EnvironmentVariables
+    {
+        public static string AccountSid = "ACb7801c7dfea5c728f5c527cda72cfaee";
+        public static string AuthToken = "888e04545f08af3379f9fbc51c019441";
+    }
+}
