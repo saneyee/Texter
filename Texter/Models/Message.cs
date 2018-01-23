c136ad5a1c4e0a0dec5d04130c65b9cbc5abcacc
saneyee sarkar <saneyeesarkar@gmail.com>
2018-01-23 10:32:51 -0800

    models have message class

---
+++ Texter/Models/Message.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using RestSharp;
+using RestSharp.Authenticators;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Texter.Models
+{
+    public class Message
+    {
+        public string To { get; set; }
+        public string From { get; set; }
+        public string Body { get; set; }
+        public string Status { get; set; }
+
+
+
+        public static List<Message> GetMessages()
+        {
+            var client = new RestClient("https://api.twilio.com/2010-04-01");
+            var request = new RestRequest("Accounts/ACb7801c7dfea5c728f5c527cda72cfaee/Messages.json", Method.GET);
+            client.Authenticator = new HttpBasicAuthenticator("ACb7801c7dfea5c728f5c527cda72cfaee", "888e04545f08af3379f9fbc51c019441");
+            var response = new RestResponse();
+            Task.Run(async () =>
+            {
+                response = await GetResponseContentAsync(client, request) as RestResponse;
+            }).Wait();
+            JObject jsonResponse = JsonConvert.DeserializeObject<JObject>(response.Content);
+            var messageList = JsonConvert.DeserializeObject<List<Message>>(jsonResponse["messages"].ToString());
+            return messageList;
+        }
+
+        public static Task<IRestResponse> GetResponseContentAsync(RestClient theClient, RestRequest theRequest)
+        {
+            var tcs = new TaskCompletionSource<IRestResponse>();
+            theClient.ExecuteAsync(theRequest, response => {
+                tcs.SetResult(response);
+            });
+            return tcs.Task;
+        }
+
+        public void Send()
+        {
+            var client = new RestClient("https://api.twilio.com/2010-04-01");
+            var request = new RestRequest("Accounts/ACb7801c7dfea5c728f5c527cda72cfaee/Messages", Method.POST);
+            request.AddParameter("To", To);
+            request.AddParameter("From", From);
+            request.AddParameter("Body", Body);
+            client.Authenticator = new HttpBasicAuthenticator("ACb7801c7dfea5c728f5c527cda72cfaee", "888e04545f08af3379f9fbc51c019441");
+            client.ExecuteAsync(request, response => {
+                Console.WriteLine(response.Content);
+            });
+        }
+    }
+}
